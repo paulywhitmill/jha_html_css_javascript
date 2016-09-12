1bf3c9a2d9da0f372f2807ac0d6e1680fdc93ee6
paulywhitmill <paulywhitmill@yahoo.com>
2016-09-12 07:35:27 +0200

    Module4 Solution

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,21 @@
+
+(function (window) {
+
+  var byeSpeaker ={};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+    document.write(speakWord + " " + name, "<br/>");
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+
+
+})(window);
+
+
+
+
+
+	
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,17 @@
+
+(function (window) {
+
+  var helloSpeaker ={};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+    document.write(speakWord + " " + name, "<br/>");
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+
+
+})(window);
+
+
